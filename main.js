589d064418d035fc73d3803927a48d0e806fb77e
Adele <degottardia>
2025-06-04 09:39:53 +0200

    funzione per calcolare età,  aggiornato nome, età, descrizione

--- main.js
+++ main.js
@@ -11,28 +11,63 @@ async function getPetsData() {
 async function displayPets() {
 
     const pets = await getPetsData();
+    // console.log(pets)
     const template = document.querySelector('#animal-card-template')
-    
+
     const wrapper = document.querySelector('main')
     
     console.log(template)
 
-    pets.forEach( pet => {
-        const clone = template.content.cloneNode(true)
-
-        // qui modifichiamo il template
-        // aggiorniamo la foto
-        const image = clone.querySelector('.animal-card-photo img')
-        image.src = pet.photo
-        // console.log(pet.photo)
-        const name = clone.querySelector('.animal-card-text')
-        name.textContent = pet.name      
+     function AgeCalculator(pet) {
+            const current_date= new Date()
+            const fullYear= current_date.getFullYear()
+            const age = fullYear - pet.birthYear 
+            if (age ==1){return "less than a year old" }
+            if (age == 0){return "newborn"}
+            if (age < 1){return age +" year old"}
+            if (age >1){return age + " years old"} };
         
-        //aggiungiamo l'articolo alla pagina
-        wrapper.appendChild(clone)
+  
+
+    pets.forEach( pet => {
+        // Creo la costante clone:
+        const clone = template.content.cloneNode(true)
+        
+        //Modifichiamo il template
+
+
+        // Modifico il nome:
+        const name = clone.querySelector('.animal-card-name')
+        name.textContent = pet.name;
+
+        //Modifico l'età:
+        const age = clone.querySelector('.animal-card-age')
+        console.log(AgeCalculator(pet))
+        age.textContent = AgeCalculator(pet)
+
+    
+        //Modifico il tipo di animale:
+        const species = clone.querySelector('.animal-card-species')
+        species.textContent = pet.species
+    
+
+        //Modifico la foto:
+        const image = clone.querySelector('.animal-card-photo img')
+        image.src = pet.photo;
+                                    // console.log(pet.photo)
+         
+        //Modifico la descrizione:
+        const description= clone.querySelector('.animal-card-description')
+        description.textContent = pet.description;
+        
+       
+        //Aggiungo l'articolo alla pagina
+        wrapper.appendChild(clone);
 
         }    
     )
 }
 
 displayPets()
+
+// https://frapollif.github.io/pet-adoption-data/pets.json 
